c1c783dd3a63a07e1785f47fa67f44969aa0cd51
Amitav Roy <reachme@amitavroy.com>
2021-10-05 21:53:20 +0530

    Adding some proper patient data and fixed the last visit date format.

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,10 +11,15 @@ class Patient extends Model
     use HasFactory;
 
     protected $fillable = [
-        'patient_id', 'name', 'phone_number', 'location', 'year_of_birth', 'weight', 'visit_count', 'last_visit'
+        'patient_id', 'name', 'phone_number', 'location', 'year_of_birth', 'weight', 'visit_count', 'last_visit',
     ];
 
     protected $casts = [
-        'last_visit' => 'timestamp',
+        'last_visit' => 'date',
     ];
+
+    public function getLastVisitAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d');
+    }
 }
--- database/seeders/DefaultData.php
+++ database/seeders/DefaultData.php
@@ -21,8 +21,9 @@ class DefaultData extends Seeder
         ]);
 
         $this->createLocations();
+        $this->createPatients();
 
-        Patient::factory(50)->create();
+        // Patient::factory(50)->create();
     }
 
     private function createLocations()
@@ -30,19 +31,52 @@ class DefaultData extends Seeder
         Location::create([
             'name' => 'Fortis',
             'short_address' => 'Vashi, Navi Mumbai',
-            'type' => 'HOSPITAL'
+            'type' => 'HOSPITAL',
         ]);
 
         Location::create([
             'name' => 'Apollo',
             'short_address' => 'Belapur, Navi Mumbai',
-            'type' => 'HOSPITAL'
+            'type' => 'HOSPITAL',
         ]);
 
         Location::create([
             'name' => 'Matrix clinix',
             'short_address' => 'Nerul, Navi Mumbai',
-            'type' => 'HOSPITAL'
+            'type' => 'HOSPITAL',
+        ]);
+    }
+
+    public function createPatients()
+    {
+        Patient::create([
+            'patient_id' => '20211001',
+            'name' => 'Murali Manohar',
+            'phone_number' => '6852457891',
+            'location' => 'Navi Mumbai',
+            'year_of_birth' => 1985,
+            'weight' => 85,
+            'visit_count' => 5,
+        ]);
+
+        Patient::create([
+            'patient_id' => '20211002',
+            'name' => 'Manish Sawant',
+            'phone_number' => '6852466891',
+            'location' => 'Mumbai',
+            'year_of_birth' => 1975,
+            'weight' => 70,
+            'visit_count' => 2,
+        ]);
+
+        Patient::create([
+            'patient_id' => '20211003',
+            'name' => 'Dinesh Kumar',
+            'phone_number' => '4552466891',
+            'location' => 'Vashi',
+            'year_of_birth' => 1995,
+            'weight' => 55,
+            'visit_count' => 1,
         ]);
     }
 }
